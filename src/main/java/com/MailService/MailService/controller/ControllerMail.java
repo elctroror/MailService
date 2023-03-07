d6eba16fcc8ec1bfb8982a595a146a93a7ebd345
DIR\joel.favero <joel.favero@accenture.com>
2023-03-07 12:43:48 -0300

    MailService v1.1

--- src/main/java/com/MailService/MailService/controller/ControllerMail.java
+++ src/main/java/com/MailService/MailService/controller/ControllerMail.java
@@ -7,10 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("mail")
 public class ControllerMail {
 
     @Autowired
